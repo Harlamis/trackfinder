d0471cca0ca2470f4792e3b076f35b899fce58da
Yeghor Danyliuk <egordan74@gmail.com>
2026-01-07 17:05:50 +0200

    add App component

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./index.css"
+
+function App() {
+    return (
+        <div className="min-h-screen p-12 sm:p-6 bg-main-bg flex items-center justify-center">
+            <h1 className="text-pf-accent">Welcome to Trackfinder!</h1>
+        </div>
+    )
+}
+
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import App from './App'
 
 createRoot(document.getElementById('root')!).render(
-  <h1>Hello World!</h1>
+  <App />
 )
