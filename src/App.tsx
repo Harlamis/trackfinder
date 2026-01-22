7e01687102c3a0a6a793b0b9c4f5733c18ef28a0
Yeghor <egordan74@gmail.com>
2026-01-22 23:14:31 +0200

    refactor App and EncounterTable components to use ParticipantCard, update TypeScript target to ES2023

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,7 @@ import "./index.css";
 function App() {
   return (
     <div className="min-h-screen p-12 sm:p-6 bg-main-bg flex items-center justify-center flex-col">
-      <h1 className="text-pf-accent font-display">
-        Welcome to Trackfinder!
-        <EncounterParticipant />
-      </h1>
+      <h1 className="text-pf-accent font-display">Welcome to Trackfinder!</h1>
       <EncounterTable />
     </div>
   );
--- src/components/EncounterTable.tsx
+++ src/components/EncounterTable.tsx
@@ -1,71 +1,22 @@
 import "../index.css";
 import { Button } from "./Button";
 import ConditionBadge from "./ConditionBadge";
-
-const tableHeaders = [
-  "Innitiative",
-  "HP",
-  "AC",
-  "Conditions",
-  "Comments",
-  "Actions",
-];
-
-type Condition = string;
-type Comment = string;
-
-interface IParticipant {
-  initiative: number;
-  hp: number;
-  ac: number;
-  conditions: Condition[];
-  comments: Comment[];
-}
+import ParticipantCard from "./ParticipantCard";
+import { type IParticipant } from "./ParticipantCard";
 
 const participants: IParticipant[] = [
-  {
-    initiative: 10,
-    hp: 45,
-    ac: 20,
-    conditions: ["some condition"],
-    comments: ["some comment"],
-  },
+  { base_id: 1, name: "John", health: 45, armor_class: 19, initiative: 18 },
+  { base_id: 2, name: "Joe", health: 23, armor_class: 14, initiative: 20 },
 ];
+
 export function EncounterTable() {
   return (
-    <table className="bg-main-card border border-amber-600">
-      <thead>
-        <tr className="border-b border-amber-600">
-          {tableHeaders.map((header) => (
-            <th
-              key={header}
-              className="text-pf-accent text-left p-4 font-bold uppercase text-sm"
-            >
-              {header}
-            </th>
-          ))}
-          <th>
-            <Button>Hello World!</Button>
-          </th>
-        </tr>
-      </thead>
-      <tbody>
-        {participants.map(
-          ({ initiative, hp, ac, conditions, comments }, index) => (
-            <tr key={index} className="border-b border-amber-400">
-              <td className="p-4 text-pf-accent font-mono font-bold">
-                {initiative}
-              </td>
-              <td className="p-4 text-pf-red">{hp}</td>
-              <td className="p-4 text-">{ac}</td>
-              <td className="p-4">
-                {conditions} <ConditionBadge type="Stunned" />
-              </td>
-              <td className="p-4">{comments}</td>
-            </tr>
-          )
-        )}
-      </tbody>
-    </table>
+    <div className="flex flex-col h-fit w-5xl">
+      {participants
+        .toSorted((a, b) => b.initiative - a.initiative)
+        .map((participant) => (
+          <ParticipantCard key={participant.base_id} {...participant} />
+        ))}
+    </div>
   );
 }
