2c2ec3742b4986b4e309400d21cd49d42f6f27f8
Yeghor Danyliuk <egordan74@gmail.com>
2026-06-26 22:53:13 +0300

    add gaps in encounterTable

--- src/components/EncounterTable.tsx
+++ src/components/EncounterTable.tsx
@@ -30,8 +30,8 @@ export const EncounterTable = ({ encounter }: EncounterTableProps) => {
   return (
     <section className='flex flex-col md:col-span-6'>
       <div className='w-full text-2xl text-text-muted'>
-        <h2>Round: {encounter.currentRound}</h2>
-        <div>
+        <h2 className='w-full text-center'>Round: {encounter.currentRound}</h2>
+        <div className='flex flex-col gap-5'>
           <div className='hidden grid-cols-12 gap-4 px-4 py-2 text-text-main md:grid'>
             <div className='col-span-1'>Init</div>
             <div className='col-span-5'>Name</div>
