c28ba0fef8fd4d65f9e705048ab4fe9f4627197f
Yeghor Danyliuk <egordan74@gmail.com>
2026-06-27 12:58:39 +0300

    fix encounter table layout; add monster sorting before rendering EncounterTable

--- src/components/EncounterTable.tsx
+++ src/components/EncounterTable.tsx
@@ -12,8 +12,11 @@ interface EncounterTableRowProps {
 const EncounterTableRow = ({ monster }: EncounterTableRowProps) => {
   return (
     <div className='flex flex-col gap-3 rounded-lg bg-main-card p-4 md:grid md:grid-cols-12 md:gap-4 md:bg-transparent md:px-4 md:py-2 md:text-text-main'>
-      <div className='col-span-1'>{monster.init}</div>
-      <div className='col-span-5'>{monster.name}</div>
+      <div className='flex items-center justify-between md:contents'>
+        <div className='col-span-1'>{monster.init}</div>
+        <div className='col-span-5'>{monster.name}</div>
+        <div className='col-span-1'>{monster.ac}</div>
+      </div>
       <div className='col-span-5'>
         <Healthbar
           currentHp={monster.currentHp}
@@ -21,7 +24,6 @@ const EncounterTableRow = ({ monster }: EncounterTableRowProps) => {
           isPlayer={monster.isPlayer}
         />
       </div>
-      <div className='col-span-1'>{monster.ac}</div>
     </div>
   );
 };
@@ -35,12 +37,14 @@ export const EncounterTable = ({ encounter }: EncounterTableProps) => {
           <div className='hidden grid-cols-12 gap-4 px-4 py-2 text-text-main md:grid'>
             <div className='col-span-1'>Init</div>
             <div className='col-span-5'>Name</div>
-            <div className='col-span-5'>HP</div>
             <div className='col-span-1'>AC</div>
+            <div className='col-span-5'>HP</div>
           </div>
-          {encounter.monsters.map((mon) => (
-            <EncounterTableRow monster={mon} key={mon.id} />
-          ))}
+          {encounter.monsters
+            .toSorted((a, b) => b.init - a.init)
+            .map((mon) => (
+              <EncounterTableRow monster={mon} key={mon.id} />
+            ))}
         </div>
       </div>
     </section>
