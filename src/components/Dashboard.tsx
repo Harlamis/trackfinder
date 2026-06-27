ee14f25b393e1453ab46667fe9242550c995c07d
Yeghor <egordan74@gmail.com>
2026-06-27 18:10:23 +0300

    move mock data to separate file

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -4,86 +4,9 @@ import { Sidebar } from './Sidebar';
 import { type Encounter } from '../types';
 import type { SidebarMonsterMetadata } from '../types';
 import { EncounterTable } from './EncounterTable';
+import { INITIAL_ENCOUNTERS } from '../MockData';
 
-const INITIAL_ENCOUNTERS: Encounter[] = [
-  {
-    id: 1,
-    name: 'Ambush on the High Road',
-    currentRound: 1,
-    activeMonsterId: 101,
-    monsters: [
-      {
-        id: 101,
-        name: 'Tordin (Paladin)',
-        maxHp: 25,
-        currentHp: 20,
-        ac: 18,
-        init: 18,
-        isPlayer: true,
-      },
-      {
-        id: 102,
-        name: 'Goblin Archer A',
-        maxHp: 12,
-        currentHp: 12,
-        ac: 13,
-        init: 15,
-        isPlayer: false,
-      },
-      {
-        id: 103,
-        name: 'Goblin Boss',
-        maxHp: 30,
-        currentHp: 24,
-        ac: 15,
-        init: 11,
-        isPlayer: false,
-      },
-      {
-        id: 104,
-        name: 'Elyon (Ranger)',
-        maxHp: 18,
-        currentHp: 14,
-        ac: 16,
-        init: 6,
-        isPlayer: true,
-      },
-    ],
-  },
-  {
-    id: 2,
-    name: "Ogre's Den",
-    currentRound: 3,
-    activeMonsterId: 105,
-    monsters: [
-      {
-        id: 105,
-        name: 'Cave Ogre',
-        maxHp: 59,
-        currentHp: 20,
-        ac: 12,
-        init: 8,
-        isPlayer: false,
-      },
-      {
-        id: 101,
-        name: 'Tordin (Paladin)',
-        maxHp: 25,
-        currentHp: 25,
-        ac: 18,
-        init: 14,
-        isPlayer: true,
-      },
-    ],
-  },
-  {
-    id: 3,
-    name: 'Empty Encounter Test',
-    currentRound: 1,
-    activeMonsterId: null,
-    monsters: [],
-  },
-];
+
 
 export const Dashboard = () => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
