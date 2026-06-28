0f5b5db008b474445e7840e498479c5a60370e6a
Yeghor <egordan74@gmail.com>
2026-06-28 12:01:32 +0300

    add MonsterDetails interface and related types

--- src/types.tsx
+++ src/types.tsx
@@ -14,10 +14,44 @@ export interface Monster {
   ac: number;
   init: number;
   isPlayer: boolean;
+  details?: MonsterDetails;
 }
 export type SidebarMonsterMetadata = Omit<
   Monster,
   'maxHp' | 'currentHp' | 'ac' | 'init' | 'isPlayer'
 >;
+
+export interface Strike {
+  name: string;
+  type: 'melee' | 'ranged';
+  bonus: number;
+  traits: string[];
+}
+
+export interface Loot {
+  name: string;
+  quantity: number;
+  value?: number;
+}
+
+export interface MonsterDetails {
+  level: number;
+  traits: string[];
+  speed: {
+    walking: number;
+    flying: number;
+    swimming: number;
+    borrrowing: number;
+    climbing: number;
+  };
+  savingThrows: {
+    fortitude: number;
+    reflex: number;
+    will: number;
+  };
+
+  attacks: Strike[];
+  loot: Loot[];
+}
 
 export type PanelMode = 'closed' | 'bestiary' | 'inspector';
